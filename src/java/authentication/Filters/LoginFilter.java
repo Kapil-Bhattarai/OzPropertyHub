9b09896160e011cfa1f5671f8f171186a35c3c3c
YubarajC <yubarajchauhan344@gmail.com>
2023-08-24 20:21:15 +1000

    add: signout added

--- src/java/authentication/Filters/LoginFilter.java
+++ src/java/authentication/Filters/LoginFilter.java
@@ -11,6 +11,7 @@ import jakarta.servlet.ServletResponse;
 import jakarta.servlet.annotation.WebFilter;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import oz.user.UserController;
 
 @WebFilter("/*")
 public class LoginFilter implements Filter {
@@ -22,12 +23,12 @@ public class LoginFilter implements Filter {
     public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
        HttpServletRequest req = (HttpServletRequest) request;
        HttpServletResponse resp = (HttpServletResponse) response;
-       //Retrieve the authtication bean
-       AutenticationBean session = (AutenticationBean) req.getSession().getAttribute("authBean");
-       //Any after login accessible pages should be listed here
-       String[] afterLog = {"logout.faces", "default.faces", "admin_dashboard.faces", "agent_dashboard.faces", "user_dashboard.faces"};
+      
+       UserController session = (UserController) req.getSession().getAttribute("userBean");
+        System.out.println("session value "+session);
+       String[] afterLog = {"logout.faces?faces-redirect=true", "default.faces?faces-redirect=true", "admin_dashboard.faces?faces-redirect=true", "agent_dashboard.faces?faces-redirect=true", "user_dashboard.faces?faces-redirect=true"};
        String url=req.getRequestURI();
-       if (session==null || !session.isLogged()) {
+       if (session==null || !session.isLoggedIn()) {
            boolean risk=false;
            for (String afterLog1 : afterLog) {
                if (url.contains(afterLog1)) {
@@ -48,7 +49,7 @@ public class LoginFilter implements Filter {
                resp.sendRedirect(req.getServletContext().getContextPath()+"/default.faces");
            }
            else if (url.contains("logout.faces")) {
-               req.getSession().removeAttribute("authBean");
+               req.getSession().removeAttribute("userBean");
                resp.sendRedirect(req.getServletContext().getContextPath()+"/login.faces");
            }
            else {
