235f69914f8b49f999956da3ace97019139c2759
Kapil Bhattarai <kapil.edu.bhattarai@gmail.com>
2023-09-23 18:50:57 +1000

    Add: Message Type added

---
+++ src/java/oz/MessageType.java
@@ -0,0 +1,25 @@
+package oz;
+
+
+public enum MessageType {
+    
+    MESSAGE_PENDING_REQUEST("Message for Pending Request"),
+    MESSAGE_ACCOUNT_SUSPENSION("Message for Account Suspension"),
+    MESSAGE_ACCOUNT_DELETION("Message for Account deletion"),
+    MESSAGE_ACCOUNT_ACTIVATION("Message for Account activation"),
+    
+    ERROR_USER_NOT_FOUND("Error Message for User Not Found"),
+    ERROR_PROFILE_FETCH_ERROR("Error Message for Profile Fetch Error"),
+    ERROR_WHILE_SENDING_EMAIL("Error Message while sending email");
+    
+    private  String displayName;
+
+    private MessageType(String displayName) {
+        this.displayName = displayName;
+    }
+
+    public String getDisplayName() {
+        return displayName;
+    }
+
+}
